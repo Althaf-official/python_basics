08e9bb9bca4f15b0d5e047817ccef861f3eb7dd6
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-06-26 14:10:57 +0400

    average of three numbers

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-a=input("enter  two number")
-b=input()
-
-print("entered first number  is  "+(a) +"and second number is "+(b))
-
-
-sum=int(a)+int(b)
-
-print("sum of two number is "+str(sum))
-print("hello world")
-print("this is anothere text")
+num1=input("enter 3 numbers")
+num2=input()
+num3=input()
+average=(int (num1)+int (num2)+int (num3))/3
+print("average of 3 numbers is  :"+str(average))
