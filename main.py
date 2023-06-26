327b92676d3d1f7d5a2599060dcecf3d05dfaa21
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-06-26 12:20:46 +0400

    sum of two numbers

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 a=input("enter  two number")
 b=input()
 
-print("entered first number  is  "+str(a) +"and second number is "+str(b))
+print("entered first number  is  "+(a) +"and second number is "+(b))
 
 
+sum=int(a)+int(b)
+
+print("sum of two number is "+str(sum))
