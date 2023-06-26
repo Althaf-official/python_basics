375d2f9d13c5e78a0b0a6a33114ab24817f84288
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-06-26 14:25:24 +0400

    multiline comment

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 num1=int(input("enter two numbers"))
 num2=int(input())
 
+
+h="""this is
+multi line 
+comment
+"""
 print(type(num1))#this is sample comment
 temp=num1
 num1=num2
 num2=temp
 
 print("after swaping first num is :"  +str(num1)+"second number is "+str(num2))
+
+print(h)
