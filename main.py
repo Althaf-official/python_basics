520d7963f8928bb97e1649948be71b60159619e1
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-06-27 19:52:03 +0400

    sample commit

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-num1=int(input("enter two numbers"))
-num2=int(input())
+a="this_is_mascreation"
 
-
-h="""this is
-multi line 
-comment
-"""
-print(type(num1))#this is sample comment
-temp=num1
-num1=num2
-num2=temp
-
-print("after swaping first num is :"  +str(num1)+"second number is "+str(num2))
-
-print(h)
+print(len(a))
+print(a[4:17])
