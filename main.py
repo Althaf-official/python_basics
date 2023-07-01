23ed0a730741cc433240a562a9c5f327b92233a9
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-01 12:49:30 +0400

    One Value to Multiple Variables

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-x, y, z,mas  = "Orange", "Banana", "Cherry","jam"
-
+x = y = z = "Orange"
 print(x)
 print(y)
 print(z)
-print(mas)
-
-#Make sure the number of variables matches the number of values, or else you will get an error.
