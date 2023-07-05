0273b354fc81d3da6b8727e23a0e56c1a71e0db8
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-05 15:09:06 +0400

    For loop

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 names=["Muhammed", "Althaf","Sharafudheen","Jaseela"]
 print(names)
 names.append("ashraf")
-names.pop(1)
+names.pop(1)#remove first position Althaf
 names.remove("Muhammed")
 
 for x in names:
