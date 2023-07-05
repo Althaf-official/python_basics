7fe1ed9f022bbeddf7ec79ba2ac12e5e43652d5c
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-05 15:23:27 +0400

    forloop multiplication table using forloop range

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 num = int(input("Enter a number: "))
 
 print(f"Multiplication table for {num}:")
+string_ = """In Python, using curly braces {} within a string is a way to perform string formatting or interpolation. It allows you to embed values or expressions inside a string.
 
-for i in range(1, 11):
+The notation {num} within a string is a placeholder that represents a variable or value that will be replaced during string formatting. The num in {num} refers to a variable name or expression that you want to substitute into the string."""
+
+for i in range(1, 19):
+    print(i)
     product = num * i
     print(f"{num} x {i} = {product}")
