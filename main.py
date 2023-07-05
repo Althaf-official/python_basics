bd23de201337c4745b3de977e9ddde3b42649dca
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-05 14:12:31 +0400

    ifElse number is negative or positive

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ num=int(input("Enter a number"))
 
 if num<0:
     print("Enterd Number is negative")
+elif num==0:
+    print("Entered number is zero")
 else:
     print("Entered Number is Positive")
