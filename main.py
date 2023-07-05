33f2c1e97b89c3a9039aba6d6df705d9ddd70d31
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-05 14:45:45 +0400

    end ekeyword in pythonIn this example, the first print() statement ends with a space character (end=" ") instead of the default newline character. The second print() statement follows on the same line and ends with an exclamation mark (end="!").
    By modifying the end parameter, you have flexibility in controlling the output format when using the print() function.

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
-def print_star_pyramid(rows):
-    # Initialize variables
-    i = 0
-    while i < rows:
-        # Print spaces
-        j = 0
-        while j < rows - i - 1:
-            print(" ", end="")
-            j += 1
+print("Hello", end=" World  ")
 
-        # Print stars
-        j = 0
-        while j <= i:
-            print("* ", end="")
-            j += 1
+print("world", end="!")
 
-        # Move to the next line
-        print()
-        i += 1
-
-
-# Test the function
-rows = int(input("Enter the number of rows: "))
-print_star_pyramid(rows)
