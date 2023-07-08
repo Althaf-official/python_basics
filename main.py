8d6476d9862377e889aef789c595982efbfa1428
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-08 14:23:54 +0400

    Dictionary Make a change in the original dictionary, and see that the values list gets updated as well

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
 }
-x = thisdict.values()
-print(x)
+
+x = car.values()
+
+print(x) #before the change
+
+car["year"] = 2020
+
+print(x) #after the change
