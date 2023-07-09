261376a3bcd6e9ed7ac1fe700ef7d756644e92c1
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-09 19:59:36 +0400

    module math

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import platform
+from math import isinf
 
-print(platform.system())
-print(platform.machine())
-print(platform.platform())
-print(platform.release())
-print(platform.version())
-print(platform.architecture())
-#print(platform.dist())
-#print(platform.freedesktop_os_release())
-print(platform.java_ver())
-print(platform.node())
+
+print(isinf(-0))
