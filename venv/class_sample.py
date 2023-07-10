7809a369b144ef52b17c6001f07f3b9de3d7b456
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-10 13:02:38 +0400

    OOPS explained

---
+++ venv/class_sample.py
@@ -0,0 +1,17 @@
+#created a class
+class My_Sample_Class():
+    def sample_function(self,name):
+        print(name)
+
+
+#created a object for class
+x=My_Sample_Class()
+
+#calling function from a object
+x.sample_function()
+#My_Sample_Class.sample_function(x)
+"""
+instead of like this 
+we can call the function like this also
+My_sample_Class.sample_function(x)
+"""
