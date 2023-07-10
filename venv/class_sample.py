70957d525afcf3a34146326fbb4a092343950578
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-10 13:14:42 +0400

    OOPS explained Class object

--- venv/class_sample.py
+++ venv/class_sample.py
@@ -1,17 +1,26 @@
 #created a class
 class My_Sample_Class():
-    def sample_function(self,name):
-        print(name)
+    def sample_function(self,n):
+        self.name=n
+
+    def print_name(self):
+        print(self.name)
+
 
 
 #created a object for class
 x=My_Sample_Class()
+y=My_Sample_Class()
 
-#calling function from a object
-x.sample_function()
-#My_Sample_Class.sample_function(x)
-"""
-instead of like this 
-we can call the function like this also
-My_sample_Class.sample_function(x)
-"""
+
+my_wifes_name="jaseela asharaf is my wifes name"
+my_name="Muhammed althaf is my name"
+
+
+# call the function and passing aurument
+x.sample_function(my_name)
+y.sample_function(my_wifes_name)
+
+#same function separate memory space
+x.print_name()
+y.print_name()
