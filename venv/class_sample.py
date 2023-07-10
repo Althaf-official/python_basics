c69ec8c818411a2269a271ad02691a281a678459
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-10 16:43:04 +0400

    OOPS Constructor  constructor is a special method within a class that is automatically called when you create an object of that class. It is used to initialize the attributes of the object. The constructor method is defined with the name __init__()

--- venv/class_sample.py
+++ venv/class_sample.py
@@ -2,12 +2,16 @@ class MyClass:
     def __init__(self, name, age):#constructor
         self.name = name
         self.age = age
-"""
-constructor is a special method within a class that is automatically called when you create an object of that class. 
-It is used to initialize the attributes of the object. The constructor method is defined with the name __init__()
-"""
+
     def say_hello(self):
         print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+
+        """
+        constructor is a special method within a class that is automatically called when you create an object of that class.
+         It is used to initialize the attributes of the object. The constructor method is defined with the name __init__()
+         
+         """
 
 # Creating an object of MyClass
 obj = MyClass("John", 25)
