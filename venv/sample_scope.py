6c7895140af56e98a990ea19da6d43c1b4dfb06c
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-10 12:16:29 +0400

    local and global varible called inside a function

--- venv/sample_scope.py
+++ venv/sample_scope.py
@@ -1,2 +1,17 @@
 def check_scope():
-    print("")
+    def do_local_test():
+        #this test is local variable .available only inside this function
+        test="local test"
+    def do_non_local_test():
+        test="non local test"
+    def do_global_test():
+        test="global test"
+
+
+#this test is global variable
+    test="Default"
+    do_local_test()
+    print("test value is   :" + test)
+
+check_scope()
+
