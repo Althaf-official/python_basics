0ba611bc3f5a31b3a45d12fdfcddfe8158dab883
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-12 19:36:10 +0400

    operator overloading

---
+++ venv/operator_overloading.py
@@ -0,0 +1,18 @@
+class FUllNAME:
+    def set_name(self,name):
+        self.name=name
+
+    def __add__(self, other):
+        name=self.name+" "+other.name
+        return name
+
+#create two object for Fullname class
+first_name=FUllNAME()
+second_name=FUllNAME()
+
+first_name.set_name("althaf")
+second_name.set_name("mas")
+
+
+full_name=first_name+second_name
+print(full_name)
