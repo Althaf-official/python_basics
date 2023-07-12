76093de6e05a15da8e8e96ceef47bad0984b72c7
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-12 18:54:38 +0400

    multilevel_inheritance_

--- venv/multilevel_inheritance_sample.py
+++ venv/multilevel_inheritance_sample.py
@@ -1,1 +1,31 @@
-print("multilevel inheritance")
+class First:
+    def display_first(self):
+        print("this is first class display function")
+
+class Second(First):
+    def display_second(self):
+        print("this is second class display function")
+
+#third class inherit first and second classes
+class Third(Second):
+    def display_third(self):
+        print("this is third class display funciton")
+
+
+#create object for Third class
+obj=Third()
+
+#calling third class display function
+obj.display_third()
+obj.display_second()
+obj.display_first()
+
+
+print("_____________________________________________________________--")
+
+#create object for second class
+obj2=Second()
+
+obj2.display_second()
+obj2.display_first()
+
