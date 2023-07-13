dd61169ef080505dc3e7bb681c8c5ac4385d9185
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-13 14:50:28 +0400

    Date time current  printed

--- venv/datetime library.py
+++ venv/datetime library.py
@@ -5,8 +5,7 @@ obj_day=datetime.datetime.now()
 print(obj_day.strftime("%d/%m/%Y"))
 print(datetime.datetime.today().month)
 print(obj_day)
-
-
 print(obj_day.time())
 print(obj_day.now())
 print(obj_day.astimezone())
+
