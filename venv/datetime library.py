5eab535093217ea7fb5fde13bfddf3b5f4ba741d
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-13 14:57:04 +0400

    date - create a programme for age finding using date of birth

--- venv/datetime library.py
+++ venv/datetime library.py
@@ -2,10 +2,15 @@ import datetime
 
 obj_day=datetime.datetime.now()
 
-print(obj_day.strftime("%d/%m/%Y"))
-print(datetime.datetime.today().month)
-print(obj_day)
-print(obj_day.time())
-print(obj_day.now())
-print(obj_day.astimezone())
+x=datetime.datetime(1995,12,27)
+y=datetime.datetime.now()
+print(y)
+print(x)
+
+
+diff=y-x;
+print("i live total  :"+str(diff.days)+" Days")
+
+years=diff.days/365
+print("My age is   : "+str(years))
 
