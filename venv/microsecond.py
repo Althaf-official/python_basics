b497658a09bcbc987d79e73e260ca5edb7e4d00f
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-13 15:21:12 +0400

    python file handling  create a file using f=open("althaf.py","w")

--- venv/microsecond.py
+++ venv/microsecond.py
@@ -1,11 +1,22 @@
-import datetime
-
-end=datetime.datetime.now()
-for i in range(20000):
-    print(i)
+f=open("althaf.py","w")
 
 
-now=datetime.datetime.now()
+"""
 
-difference=end-now
-print(difference)
+File Handling
+The key function for working with files in Python is the open() function.
+
+The open() function takes two parameters; filename, and mode.
+
+There are four different methods (modes) for opening a file:
+
+"r" - Read - Default value. Opens a file for reading, error if the file does not exist
+
+"a" - Append - Opens a file for appending, creates the file if it does not exist
+
+"w" - Write - Opens a file for writing, creates the file if it does not exist
+
+"x" - Create - Creates the specified file, returns an error if the file exists
+
+
+"""
